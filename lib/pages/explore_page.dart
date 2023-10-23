ffe9ccf0c4487324e24ec6f574abd0d8b2ba2dd8
hemanth <gohk007@gmail.com>
2023-10-23 05:57:47 +0530

    explore page details

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 
@@ -6,15 +8,55 @@ class ExplorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: ListView(
+    return Scaffold(
+        body: ListView(
       padding: EdgeInsets.all(16),
       children: [
-       Row(
-        children: [
-          Expanded(child: TextField(decoration: InputDecoration(hintText: 'Serach'),)),
-          IconButton.filled(onPressed: (){}, icon: Icon(IconlyLight.filter))
-        ],
-       )
+        Padding(
+          padding: const EdgeInsets.only(bottom: 15),
+          child: Row(
+            children: [ 
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(12),
+                      prefixIcon: Icon(IconlyLight.search),
+                      hintText: 'Serach',
+                      isDense: true,
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey.shade300),
+                          borderRadius: BorderRadius.all(Radius.circular(99)))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: IconButton.filled(onPressed: () {}, icon: Icon(IconlyLight.filter)),
+              )
+            ],
+          ),
+        ),
+        
+        //Card
+        SizedBox(height: 170,child: Card(
+          color: Colors.green.shade50,
+          elevation: 0.1,
+          shadowColor: Colors.green.shade50,
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                  Text(
+                  'Free Consultaion',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.green.shade700)),
+                  ]
+                ),
+              ],
+            ),
+          ),
+        )),
+
       ],
     ));
   }
